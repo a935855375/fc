30e35f9ff0ba7070338d92b7c85560b155f37e50
fan <935855375@qq.com>
2018-07-27 15:37:54 +0800

    init

--- src/app/page/hotnews/hotnews.module.ts
+++ src/app/page/hotnews/hotnews.module.ts
@@ -1,27 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {MaterialModule} from '../../material.module';
-import {AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
-import {FormsModule} from '@angular/forms';
 import {HotnewsRoutingModule} from './hotnews.routing.module';
 import {HotnewsComponent} from './hotnews.component';
 import {NgxPageScrollModule} from 'ngx-page-scroll';
+import {FrameModule} from '../frame/frame.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    NgbModule,
     HotnewsRoutingModule,
-    MaterialModule,
-    AngularMultiSelectModule,
-    NgxPageScrollModule
+    NgxPageScrollModule,
+    FrameModule
   ],
   declarations: [
     HotnewsComponent,
-  ]
+  ],
+
 })
 export class HotnewsModule {
 }
